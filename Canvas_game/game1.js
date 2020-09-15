ba397eac4f123ad43455a1673b8927c441d086f6
Viktoria <vika.perova.88.88@mail.ru>
2020-09-15 14:46:19 +0300

    9 уровень

--- Canvas_game/game1.js
+++ Canvas_game/game1.js
@@ -55,6 +55,8 @@ let game = {
         for (let row = 0; row < this.rows; row++) {
             for (let col = 0; col < this.cols; col++) {
                 this.blocks.push({
+                	width: 60,
+                	height: 20,
                     x: 64 * col + 65,
                     y: 24 * row + 35
                 });
@@ -64,6 +66,12 @@ let game = {
     update() {
         this.platform.move();
         this.ball.move();
+
+        for (let block of this.blocks) {
+        	if (this.ball.collide(block)) {
+        		this.ball.bumpBlock(block);
+        	}
+        }
     },
     run() {
         window.requestAnimationFrame(() => {
@@ -113,6 +121,21 @@ game.ball = {
     	if(this.dx) {
     		this.x += this.dx;
     	}
+    },
+    collide(element) {
+    	let x = this.x + this.dx;
+    	let y = this.y + this.dy;
+
+    	if (x + this.width > element.x &&
+    		x < element.x + element.width &&
+    		y + this.height > element.y &&
+    		y < element.y + element.height) {
+    			return true;
+    		}
+    	return false;
+    },
+    bumpBlock(block) {
+    	this.dy *= -1;
     }
 };
 
